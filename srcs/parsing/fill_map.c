ba45dae6a3cbb0760d50d6b034aa4d5e61b8cd13
LE Thomas <thomas.le.tle@gmail.com>
2024-05-05 17:02:31 +0200

    prettier fill struct

--- srcs/parsing/fill_map.c
+++ srcs/parsing/fill_map.c
@@ -77,12 +77,8 @@ static int	fill_texture(char *s, t_cub *cub)
 
 int	is_empty(t_cub *cub, int j, int i, char **map)
 {
-	// printf("Here : %s\n", map[i]);
 	if (!map[i][0] && j == 0 && !cub->img.map[j])
-	{
-		cub->status = fill_struct(cub, map, i + 1, j);
-		return (cub->status);
-	}
+		return (fill_struct(cub, map, i + 1, j));
 	else if (!map[i][0] && j > 0 && cub->img.map[j - 1])
 	{
 		cub->status = 2;
@@ -98,11 +94,8 @@ int	fill_struct(t_cub *cub, char **map, int i, int j)
 	if (!map[i] && !check_struct(cub, 0))
 		return (2);
 	else if (!map[i])
-	{
-		status = check_struct(cub, 1);
-		if (status)
+		if (check_struct(cub, 1))
 			return (SUCCESS);
-	}
 	if (is_texture(map[i]))
 	{
 		status = fill_texture(map[i], cub);
@@ -111,8 +104,9 @@ int	fill_struct(t_cub *cub, char **map, int i, int j)
 	}
 	else if (check_struct(cub, 2))
 	{
-		if (is_empty(cub, j, i, map) && cub->status)
-			return ((cub->status));
+		status = is_empty(cub, j, i, map);
+		if (status)
+			return (status);
 		cub->img.map[j] = map[i];
 		return (cub->img.map[j + 1] = NULL, fill_struct(cub, map, i + 1, j
 				+ 1));
